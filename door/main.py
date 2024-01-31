b9695c7e63d05926c8ff3140cf2fa280414e2575
Harry Cheung <h.m.h.cheung@lancaster.ac.uk>
2024-01-31 17:43:38 +0000

    Slight tweaks and sends messages to base station

--- door/main.py
+++ door/main.py
@@ -13,7 +13,7 @@ previousY = accelerometer.get_y()
 previousZ = accelerometer.get_z()
 speaker.on()
 radio.on()
-radio.config(length = 251, channel = 11)
+radio.config(length = 64, channel = 31)
 fill = Image("99999:" + "99999:" + "99999:" + "99999:" + "99999")
 closed = False
 closedX = 0
@@ -21,7 +21,7 @@ closedY = 0
 closedZ = 0
 set = False
 
-display.scroll("Setting door system", wait = False, loop = True, delay = 100)
+display.scroll("Setting door system", wait = False, loop = True, delay = 100) #Setup
 while (set == False):
     if (button_a.was_pressed()):
         closedX = accelerometer.get_x() #Set the MicroBit angle that the door is closed at
@@ -36,18 +36,23 @@ while True:
         x = accelerometer.get_x() #Should be using get_strength() instead but it doesn't seem to be working?
         y = accelerometer.get_y()
         z = accelerometer.get_z()
-        if (abs(closedX - x) + abs(closedY - y) + abs(closedZ - z) > 175): #If the angle deviates too much, the door is open
+        if (abs(closedX - x) + abs(closedY - y) + abs(closedZ - z) > 150): #If the angle deviates too much, the door is open
             closed = False
+            if (locked == True): #Door should not be open and locked
+                music.pitch(100, duration = -1, wait = False)
+                display.show(Image.NO, wait = False)
+            else:
+                display.clear()
         else:
             closed = True
-        if (abs(previousX - x) + abs(previousY - y) + abs(previousZ - z) > 125): #If too much change in any of the axis, count as moving
+            if (locked == False):
+                display.show(Image.SQUARE, wait = False) #Display to user that door is closed if it is not locked, indicates to user that door can be locked
+        if (abs(previousX - x) + abs(previousY - y) + abs(previousZ - z) > 110): #If too much change in any of the axis, count as moving
             state = "motion"
             if (locked == True):
                 alarm = True
-                #Placeholder to send warning to base station
-                radio.send_bytes(bytes("alarm", "utf-8"))
                 music.pitch(100, duration = -1, wait = False)
-                display.show(Image.NO)
+                display.show(Image.NO, wait = False)
         else:
             state = "stationary"
         if (locked == True):
@@ -58,14 +63,17 @@ while True:
             clos = "closed"
         else:
             clos = "open"
-        print("door " + name + " " + clos + " " + state + " " + lock)
+        if (alarm == True):
+            alar = "warning"
+        else:
+            alar = "normal"
+        print("door " + name + " " + clos + " " + state + " " + lock + " " + alar)
         #print(str(x) + " " + str(y) + " " + str(z)) #For testing
         previousX = x
         previousY = y
         previousZ = z
         last = time.ticks_ms()
-        #Placeholder to send data to base station
-        #radio.send_bytes(bytes("door " + name + " " + clos + " " + state + " " + lock, "utf-8"))
+        radio.send("002," + name + "," + lock + "," + clos + "," + alar) #Send data to base station via radio channel
     if (button_a.was_pressed() and alarm == False):
         if (locked == False and closed == True):
             locked = True
@@ -78,7 +86,5 @@ while True:
     elif (button_b.was_pressed()):
         music.stop()
         if (locked == True):
-            display.show(fill)
+            display.show(fill, wait = False)
         alarm = False
-        #Placeholder to stop warning at base station
-        #radio.send_bytes(bytes("door " + name + " alert off", "utf-8"))
