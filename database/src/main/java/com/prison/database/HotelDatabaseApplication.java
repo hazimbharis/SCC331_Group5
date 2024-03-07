ea80dae128086c2742b09269bf47bbdc6aaaaf70
Zim <98758788+hazimbharis@users.noreply.github.com>
2024-03-07 08:31:35 +0000

    made java database files extensible
    - added hotelapplication but i don't think we'll need it

--- database/src/main/java/com/prison/database/HotelDatabaseApplication.java
+++ database/src/main/java/com/prison/database/HotelDatabaseApplication.java
@@ -13,27 +13,31 @@ import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 @RestController
-public class DatabaseApplication {
+public class HotelDatabaseApplication {
+	// things to do
+	// change all references to Microbits
+	// change all query references
+
+
 	// Login variables
 
 	private static final String PASSWORD = "MyNewPass";
 	private static String URL = "jdbc:mysql://localhost:3306/?useSSL=FALSE&allowPublicKeyRetrieval=True";
-	private static String currentDatabase = "Microbits";
 	private static final String USER = "root";
 	private static Connection connection;
 
 	// Database variables
-	private static final String[] tableNames = {"lockstatus","environment","users", "prisoners", "staff", "visitors", "movement", "history", "warnings", "zonehistory", "doorhistory"};
+	private static final String[] tableNames = {"lockstatus","environment","users", "guests", "staff", "visitors", "movement", "history", "warnings", "zonehistory", "doorhistory"};
 	private static final String[] tableQuery ={
 			"doorID INT PRIMARY KEY, locked BOOL NOT NULL, closed BOOL NOT NULL, alarm BOOL NOT NULL",
 			"zoneID INT PRIMARY KEY NOT NULL, temp VARCHAR(100) NOT NULL, noise VARCHAR(100) NOT NULL, light VARCHAR(100) NOT NULL",
 			"id VARCHAR(20) PRIMARY KEY NOT NULL, firstNames VARCHAR(50) NOT NULL, lastName VARCHAR(50) NOT NULL, dob DATE NOT NULL, gender VARCHAR(10) NOT NULL, medicalConditions VARCHAR(100), type CHAR NOT NULL",
-			"id VARCHAR(20) PRIMARY KEY NOT NULL, convictions VARCHAR(100) NOT NULL, startDate DATE NOT NULL, endDate DATE, FOREIGN KEY (id) REFERENCES users(id)",
+			"id VARCHAR(20) PRIMARY KEY NOT NULL, roomType VARCHAR(100) NOT NULL, startDate DATE NOT NULL, endDate DATE, FOREIGN KEY (id) REFERENCES users(id)",
 			"id VARCHAR(20) PRIMARY KEY NOT NULL, role VARCHAR(20) NOT NULL, FOREIGN KEY (id) REFERENCES users(id)",
 			"id VARCHAR(20) PRIMARY KEY NOT NULL, phoneNo VARCHAR(11) NOT NULL, FOREIGN KEY (id) REFERENCES users(id)",
-			"prisonerID VARCHAR(100) NOT NULL, zoneID INT NOT NULL, timeStamp TIMESTAMP(3) NOT NULL, FOREIGN KEY (prisonerID) REFERENCES users(id)",
+			"guestID VARCHAR(100) NOT NULL, zoneID INT NOT NULL, timeStamp TIMESTAMP(3) NOT NULL, FOREIGN KEY (guestID) REFERENCES users(id)",
 			"id VARCHAR(100) PRIMARY KEY NOT NULL, zoneID INT NOT NULL, timeOfUse timestamp, FOREIGN KEY (id) REFERENCES users(id)",
-			"zoneID INT NOT NULL, warningID INT NOT NULL", // zone where warning occuring, type of warning: 1 assist, 2 temp, 3 temp, 4 noise, 5 light
+			"zoneID INT NOT NULL, warningID INT NOT NULL", // zone where warning occuring, type of warning: 1 staff assist, 2 temp, 3 temp, 4 noise, 5 light
 			"zoneID INT NOT NULL, temp INT NOT NULL, noise INT NOT NULL, light DOUBLE(7,2) NOT NULL, date DATE NOT NULL, time TIME NOT NULL, PRIMARY KEY(zoneID, date, time)",
 			"doorID INT NOT NULL, status VARCHAR(20) NOT NULL, date DATE NOT NULL, time TIME NOT NULL, FOREIGN KEY (doorID) REFERENCES lockstatus(doorID), PRIMARY KEY(doorID, date, time)"
 	};
@@ -46,8 +50,8 @@ public class DatabaseApplication {
 		try {
 			connection = DriverManager.getConnection(URL,USER,PASSWORD);
 			Statement stmt = connection.createStatement();
-			stmt.execute("CREATE DATABASE IF NOT EXISTS " + currentDatabase);
-			URL = "jdbc:mysql://localhost:3306/" + currentDatabase + "?useSSL=FALSE&allowPublicKeyRetrieval=True";
+			stmt.execute("CREATE DATABASE IF NOT EXISTS Hotel");
+			URL = "jdbc:mysql://localhost:3306/Hotel?useSSL=FALSE&allowPublicKeyRetrieval=True";
 			connection = DriverManager.getConnection(URL,USER,PASSWORD);
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -56,7 +60,7 @@ public class DatabaseApplication {
 
 	public static void main(String[] args) throws SQLException, MalformedURLException {
 		initialize();
-		SpringApplication.run(DatabaseApplication.class, args);
+		SpringApplication.run(HotelDatabaseApplication.class, args);
 		monitor = new SerialMonitor();
 
 		try {
@@ -67,7 +71,7 @@ public class DatabaseApplication {
 	}
 
 	private static void initialize() throws SQLException {
-		String reset = "DROP TABLE IF EXISTS " + currentDatabase + ".Realtime ";
+		String reset = "DROP TABLE IF EXISTS Hotel.Realtime ";
 		PreparedStatement stmt2 = connection.prepareStatement(reset);
 		stmt2.executeUpdate();
 		for(int i=0;i<tableNames.length;i++){
@@ -78,44 +82,77 @@ public class DatabaseApplication {
 	}
 
 	//http://localhost:8080/addPrisoner?id=AAAAAAA&zone=1
-	@GetMapping("/addPrisoner")
+	/*
+	 * 
+
+	 
+	 */
+	@GetMapping("/addGuest")
 	private void addPrisoner(@RequestParam(value = "id")String id, @RequestParam(value = "zone") int zone) throws SQLException {
 
 		/*
 		 * parameters: prisoner id (varchar), zone id (int: 1-4)
 		 *
 		 * when insert, check if ID exists
-		 * -> "SELECT * FROM " + currentDatabase + ".movement WHERE movement.item = '"+item+"'";
+		 * -> "SELECT * FROM Microbits.movement WHERE movement.item = '"+item+"'";
 		 * if true, set value of current id
 		 * if false, create new row
 		 */
-
 		// if 0, adds new prisoner entry
 		// if 1, changes value of existing prisoner entry
 		int check = 0;
 		String insertDataSQL;
 		ResultSet result;
 
-		String retrieveDataSQL = "SELECT COUNT(*) FROM " + currentDatabase + ".movement WHERE movement.prisonerID = '"+id+"'";
+		String retrieveDataSQL = "SELECT COUNT(*) FROM Hotel.movement WHERE movement.guests = '"+id+"'";
 		try (PreparedStatement statement = connection.prepareStatement(retrieveDataSQL)) {
 			result = statement.executeQuery();
 			while (result.next()) {
 				check = result.getInt(1);
 			}
-
 			if (check == 0) {
-				insertDataSQL="INSERT INTO movement (prisonerID,zoneID) VALUES (?,?)";
+				insertDataSQL="INSERT INTO movement (guestID,zoneID) VALUES (?,?)";
 				try (PreparedStatement statement1 = connection.prepareStatement(insertDataSQL)) {
 					statement1.setString(1, String.valueOf(id));
 					statement1.setString(2, String.valueOf(zone));
 					statement1.executeUpdate();
 				}
 			} else if (check == 1) {
-				insertDataSQL="UPDATE movement SET zoneID = " + zone + " WHERE prisonerID = '" + id + "'";
+				insertDataSQL="UPDATE movement SET zoneID = " + zone + " WHERE guestID = '" + id + "'";
 				try (PreparedStatement statement2 = connection.prepareStatement(insertDataSQL)) {
 					statement2.executeUpdate();
 				}
 			}
+		}
+	}
+
+		//http://localhost:8080/addPrisoner?id=AAAAAAA&zone=1
+	@GetMapping("/movementTime")
+	private void addPrisoner2(@RequestParam(value = "id")String id, @RequestParam(value = "zone") int zone) throws SQLException {
+
+		/*
+		 * parameters: prisoner id (varchar), zone id (int: 1-4)
+		 *
+		 * when insert, check if ID exists
+		 * -> "SELECT * FROM Microbits.movement WHERE movement.item = '"+item+"'";
+		 * if true, set value of current id
+		 * if false, create new row
+		 */
+
+		// if 0, adds new prisoner entry
+		// if 1, changes value of existing prisoner entry
+		String insertDataSQL;
+
+		insertDataSQL="INSERT INTO movement (guestID,zoneID, timeStamp) VALUES (?,?,?)";
+		try (PreparedStatement statement1 = connection.prepareStatement(insertDataSQL)) {
+			statement1.setString(1, String.valueOf(id));
+			statement1.setString(2, String.valueOf(zone));
+			LocalDateTime myDateObj = LocalDateTime.now();
+			DateTimeFormatter myFormatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.ms");
+			String formattedDate = myDateObj.format(myFormatObj);
+			System.out.println("After formatting: " + formattedDate);
+			statement1.setString(3, formattedDate);
+			statement1.executeUpdate();
 		}
 	}
 
@@ -129,7 +166,7 @@ public class DatabaseApplication {
 		String insertDataSQL;
 		ResultSet result;
 
-		String retrieveDataSQL = "SELECT COUNT(*) FROM " + currentDatabase + ".lockstatus WHERE lockstatus.doorID = " + door;
+		String retrieveDataSQL = "SELECT COUNT(*) FROM Hotel.lockstatus WHERE lockstatus.doorID = " + door;
 		try (PreparedStatement statement = connection.prepareStatement(retrieveDataSQL)) {
 			result = statement.executeQuery();
 			while (result.next()) {
@@ -164,7 +201,7 @@ public class DatabaseApplication {
 		String insertDataSQL;
 		ResultSet result;
 
-		String retrieveDataSQL = "SELECT COUNT(*) FROM " + currentDatabase + ".environment WHERE environment.zoneID = " + zone;
+		String retrieveDataSQL = "SELECT COUNT(*) FROM Hotel.environment WHERE environment.zoneID = " + zone;
 
 		try (PreparedStatement statement = connection.prepareStatement(retrieveDataSQL)) {
 			result = statement.executeQuery();
@@ -192,14 +229,12 @@ public class DatabaseApplication {
 
 	@GetMapping("/addWarning")
 	private void addWarning(@RequestParam(value = "zone") int zone,
-								@RequestParam(value= "warning")int warning,
-								@RequestParam(value= "noise")int noise,
-								@RequestParam(value= "light")int light) throws SQLException {
+								@RequestParam(value= "warning")int warning) throws SQLException {
 		int check = 0;
 		String insertDataSQL;
 		ResultSet result;
 
-		String retrieveDataSQL = "SELECT COUNT(*) FROM " + currentDatabase + ".warnings WHERE warnings.zoneID = " + zone + " AND warnings.warningID = " + warning;
+		String retrieveDataSQL = "SELECT COUNT(*) FROM Hotel.warnings WHERE warnings.zoneID = " + zone + " AND warnings.warningID = " + warning;
 		try (PreparedStatement statement = connection.prepareStatement(retrieveDataSQL)) {
 			result = statement.executeQuery();
 			while (result.next()) {
@@ -234,7 +269,7 @@ public class DatabaseApplication {
 		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
 		String date = datetime.format(dateFormat);
 		String time = datetime.format(timeFormat);
-		String iStatement = "INSERT INTO " + currentDatabase + ".zonehistory (zoneID, temp, noise, light, date, time) VALUES (?, ?, ?, ? ,?, ?)";
+		String iStatement = "INSERT INTO Hotel.zonehistory (zoneID, temp, noise, light, date, time) VALUES (?, ?, ?, ? ,?, ?)";
 		try (PreparedStatement pStatement = connection.prepareStatement(iStatement)) {
 			pStatement.setInt(1, zone);
 			pStatement.setInt(2, temp);
@@ -256,7 +291,7 @@ public class DatabaseApplication {
 		DateTimeFormatter timeFormat = DateTimeFormatter.ofPattern("HH:mm:ss");
 		String date = datetime.format(dateFormat);
 		String time = datetime.format(timeFormat);
-		String iStatement = "INSERT INTO " + currentDatabase + ".doorhistory (doorID, status, date, time) VALUES (?, ?, ?, ?)";
+		String iStatement = "INSERT INTO Hotel.doorhistory (doorID, status, date, time) VALUES (?, ?, ?, ?)";
 		try (PreparedStatement pStatement = connection.prepareStatement(iStatement)) {
 			pStatement.setInt(1, door);
 			pStatement.setString(2, status);
@@ -271,12 +306,12 @@ public class DatabaseApplication {
 	//http://localhost:8080/getPrisoners?zone=1
 	@GetMapping("/getPrisoners")
 	private String getPrisoners(@RequestParam(value = "zone")int zone) throws  SQLException {
-		String make = "SELECT * FROM " + currentDatabase + ".movement WHERE zoneID = " +zone;
+		String make = "SELECT * FROM Hotel.movement WHERE zoneID = " +zone;
 		PreparedStatement stmt = connection.prepareStatement(make);
 		ResultSet result = stmt.executeQuery();
 		String output = "";
 		while (result.next()){
-			String id = "PrisonerID:"+result.getString("prisonerID");
+			String id = "guestID:"+result.getString("guestID");
 			output = output.concat(String.join(",",id)+"!");
 		}
 		return output;
@@ -285,7 +320,7 @@ public class DatabaseApplication {
 	//http://localhost:8080/getDoors
 	@GetMapping("/getDoors")
 	private String getDoors() throws  SQLException {
-		String make = "SELECT * FROM " + currentDatabase + ".lockstatus";
+		String make = "SELECT * FROM Hotel.lockstatus";
 		PreparedStatement stmt = connection.prepareStatement(make);
 		ResultSet result = stmt.executeQuery();
 		String output = "";
@@ -302,7 +337,7 @@ public class DatabaseApplication {
 	//http://localhost:8080/getEnvironment?zone=1
 	@GetMapping("/getEnvironment")
 	private String getEnvironment(@RequestParam(value = "zone")int zone) throws  SQLException {
-		String make = "SELECT * FROM " + currentDatabase + ".environment WHERE zoneID = " +zone;
+		String make = "SELECT * FROM Hotel.environment WHERE zoneID = " +zone;
 		PreparedStatement stmt = connection.prepareStatement(make);
 		ResultSet result = stmt.executeQuery();
 		String output = "";
@@ -316,14 +351,6 @@ public class DatabaseApplication {
 		return output;
 	}
 
-	@GetMapping("/changeURL")
-	private String changeURL(@RequestParam(value = "url")String newUrl) throws SQLException {
-		currentDatabase = newUrl;
-		URL = "jdbc:mysql://localhost:3306/" + currentDatabase + "?useSSL=FALSE&allowPublicKeyRetrieval=True";
-		connection = DriverManager.getConnection(URL,USER,PASSWORD);
-		return "Domain successfully changed!";
-	}
-
 	@GetMapping("/setup")
 	private void webSetup() throws SQLException {
 		for (int i = 0; i < tableNames.length; i++) {
@@ -335,7 +362,7 @@ public class DatabaseApplication {
 	@GetMapping("/reset")
 	private String reset() throws SQLException {
 		for (int i = 0; i < tableNames.length; i++) {
-			String drop = "DROP TABLE IF EXISTS " + currentDatabase + "." + tableNames[i];
+			String drop = "DROP TABLE IF EXISTS Hotel." + tableNames[i];
 			PreparedStatement stmt = connection.prepareStatement(drop);
 			stmt.executeUpdate();
 		}
@@ -368,7 +395,7 @@ public class DatabaseApplication {
 
 		try (PreparedStatement statement = connection.prepareStatement(insertDataSQL)) {
 			statement.setString(1, item);
-			statement.setTimestamp(2, new java.sql.Timestamp(new java.util.Date().getTime()));
+			statement.setTimestamp(2, new Timestamp(new java.util.Date().getTime()));
 			statement.executeUpdate();
 		}
 		return "Created?";
@@ -376,7 +403,7 @@ public class DatabaseApplication {
 
 	@GetMapping("/getZD")
 	private String getZD(@RequestParam(value = "zone")int zone) throws SQLException{
-		String make = "SELECT * FROM " + currentDatabase + ".zone"+zone;
+		String make = "SELECT * FROM Hotel.zone"+zone;
 		PreparedStatement stmt = connection.prepareStatement(make);
 		ResultSet result = stmt.executeQuery();
 		String output = "";
@@ -394,9 +421,9 @@ public class DatabaseApplication {
 	private String retrieveMotionData(@RequestParam(value = "item", defaultValue = "all")String item) throws SQLException {
 		String retrieveDataSQL;
 		if(item.equals("all")){
-			retrieveDataSQL= "SELECT * FROM " + currentDatabase + ".movement";
+			retrieveDataSQL= "SELECT * FROM Hotel.movement";
 		}else{
-			retrieveDataSQL = "SELECT * FROM " + currentDatabase + ".movement WHERE movement.item = '"+item+"'";
+			retrieveDataSQL = "SELECT * FROM Hotel.movement WHERE movement.item = '"+item+"'";
 		}
 
 		PreparedStatement statement = connection.prepareStatement(retrieveDataSQL);
