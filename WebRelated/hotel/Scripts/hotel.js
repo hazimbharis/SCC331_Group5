604453069fc2175673690a972225bf10f6e38f60
said7pro <murodkhujaevs@gmail.com>
2024-03-11 01:35:44 +0000

    frontend update
    - user movement implemented
    - minor design changes
    - movement throughout rooms implemented
    - warnings implemented
    - added room data display

--- WebRelated/hotel/Scripts/hotel.js
+++ WebRelated/hotel/Scripts/hotel.js
@@ -58,31 +58,38 @@ dummyData.forEach((el) => {
 
 // Patients, doctors, visitors
 let visitor = document.createElement("i");
-visitor.classList.add("fa-solid", "fa-user", "fa-2x");
-let patient = document.createElement("i");
-patient.classList.add("fa-solid", "fa-user-injured", "fa-2x");
-let doctor = document.createElement("i");
-doctor.classList.add("fa-solid", "fa-user-doctor", "fa-2x")
+visitor.classList.add("fa-solid", "fa-user-clock", "fa-2x");
+let guest = document.createElement("i");
+guest.classList.add("fa-solid", "fa-user", "fa-2x");
+let staff = document.createElement("i");
+staff.classList.add("fa-solid", "fa-user-tie", "fa-2x")
 
 // Users Data fetched from Backend API
-const dummyUserData = [
-  {type: "P", zoneID: 1, firstName: "John", lastName: "Wick"},
-  {type: "P", zoneID: 2, firstName: "John", lastName: "Wick"},
-  {type: "P", zoneID: 3, firstName: "John", lastName: "Wick"},
-  {type: "V", zoneID: 4, firstName: "John", lastName: "Wick"},
-  {type: "V", zoneID: 5, firstName: "John", lastName: "Wick"},
-  {type: "D", zoneID: 6, firstName: "John", lastName: "Wick"},
-  {type: "P", zoneID: 7, firstName: "John", lastName: "Wick"},
-  {type: "P", zoneID: 8, firstName: "John", lastName: "Wick"},
-  {type: "D", zoneID: 9, firstName: "John", lastName: "Wick"},
-  {type: "P", zoneID: 10, firstName: "John", lastName: "Wick"},
-  {type: "D", zoneID: 11, firstName: "John", lastName: "Wick"},
-  {type: "V", zoneID: 12, firstName: "John", lastName: "Wick"},
-];
+const dummyUserData = [{
+  type: "G", zoneID: 1, firstName: "John", lastName: "Wick"
+}, {type: "G", zoneID: 2, firstName: "John", lastName: "Wick"}, {
+  type: "G", zoneID: 3, firstName: "John", lastName: "Wick"
+}, {type: "V", zoneID: 4, firstName: "John", lastName: "Wick"}, {
+  type: "V", zoneID: 5, firstName: "John", lastName: "Wick"
+}, {type: "S", zoneID: 6, firstName: "John", lastName: "Wick"}, {
+  type: "G", zoneID: 7, firstName: "John", lastName: "Wick"
+}, {type: "G", zoneID: 8, firstName: "John", lastName: "Wick"}, {
+  type: "S", zoneID: 9, firstName: "John", lastName: "Wick"
+}, {type: "G", zoneID: 10, firstName: "John", lastName: "Wick"}, {
+  type: "S", zoneID: 11, firstName: "John", lastName: "Wick"
+}, {type: "V", zoneID: 12, firstName: "John", lastName: "Wick"}, {
+  type: "S", zoneID: 12, firstName: "Wick", lastName: "Shelby"
+}];
 
 let zone1 = document.getElementById("left-side");
 let zone2 = document.getElementById("middle");
 let zone3 = document.getElementById("right-side");
+let zone1Counter = 0;
+let zone2Counter = 0;
+let zone3Counter = 0;
+let zone1CounterElement = document.getElementById("left-side-counter");
+let zone2CounterElement = document.getElementById("middle-counter");
+let zone3CounterElement = document.getElementById("right-side-counter");
 
 dummyUserData.forEach((user) => {
   let userDiv = document.createElement("div");
@@ -92,12 +99,14 @@ dummyUserData.forEach((user) => {
   userInfo.innerHTML = `Name: ${user.firstName} ${user.lastName}`;
   userDiv.appendChild(userInfo);
   if (user.zoneID <= 10) {
+    zone1Counter += 1;
+    zone1CounterElement.innerHTML = `Hotel Rooms: ${zone1Counter}`;
     switch (user.type) {
-      case "P":
-        userDiv.appendChild(patient.cloneNode(true));
+      case "G":
+        userDiv.appendChild(guest.cloneNode(true));
         break;
-      case "D":
-        userDiv.appendChild(doctor.cloneNode(true));
+      case "S":
+        userDiv.appendChild(staff.cloneNode(true));
         break;
       case "V":
         userDiv.appendChild(visitor.cloneNode(true));
@@ -105,12 +114,14 @@ dummyUserData.forEach((user) => {
     }
     zone1.appendChild(userDiv);
   } else if (user.zoneID === 11) {
+    zone2Counter += 1;
+    zone2CounterElement.innerHTML = `Reception: ${zone2Counter}`;
     switch (user.type) {
-      case "P":
-        userDiv.appendChild(patient.cloneNode(true));
+      case "G":
+        userDiv.appendChild(guest.cloneNode(true));
         break;
-      case "D":
-        userDiv.appendChild(doctor.cloneNode(true));
+      case "S":
+        userDiv.appendChild(staff.cloneNode(true));
         break;
       case "V":
         userDiv.appendChild(visitor.cloneNode(true));
@@ -118,12 +129,14 @@ dummyUserData.forEach((user) => {
     }
     zone2.appendChild(userDiv);
   } else if (user.zoneID === 12) {
+    zone3Counter += 1;
+    zone3CounterElement.innerHTML = `Restaurant: ${zone3Counter}`;
     switch (user.type) {
-      case "P":
-        userDiv.appendChild(patient.cloneNode(true));
+      case "G":
+        userDiv.appendChild(guest.cloneNode(true));
         break;
-      case "D":
-        userDiv.appendChild(doctor.cloneNode(true));
+      case "S":
+        userDiv.appendChild(staff.cloneNode(true));
         break;
       case "V":
         userDiv.appendChild(visitor.cloneNode(true));
@@ -131,4 +144,28 @@ dummyUserData.forEach((user) => {
     }
     zone3.appendChild(userDiv);
   }
-})
+});
+
+
+//Warnings
+async function showAlerts() {
+  const zones = ["Room 1", "Room 2", "Room 3", "Room 4", "Room 5", "Room 6", "Room 7", "Room 8", "Room 9", "Room 10", "Reception", "Restaurant"]
+  const warnings = ["Staff needs help in ", "Temperature too high in ", "Temperature too low in ", "Noise too high in ", "Lights too low in "]
+
+  //Fetched from API
+  let warningData = [{zoneID: 1, warningID: 1}, {zoneID: 12, warningID: 3}]
+
+  for (const warning of warningData) {
+    await Swal.fire({
+      title: "Warning!",
+      text: `${warnings[warning.warningID - 1]}${zones[warning.zoneID - 1]}`,
+      icon: "error",
+      iconColor: "#7D0A0A",
+      color:"#124076",
+      background: "#fff",
+      confirmButtonColor: "#7D0A0A"
+    })
+  }
+}
+
+showAlerts();
