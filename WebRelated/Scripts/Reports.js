810c6a6a6dcf096b7052c03fa55a4f0b6f4105c4
Harry Cheung <h.m.h.cheung@lancaster.ac.uk>
2024-03-12 02:21:03 +0000

    Bug fixes some report formatting

--- WebRelated/Scripts/Reports.js
+++ WebRelated/Scripts/Reports.js
@@ -55,8 +55,10 @@ function generate() {
         document.getElementById("feedback").textContent = "";
         document.getElementById("sDate").textContent = "Start date: " + sDate;
         document.getElementById("eDate").textContent = "End date: " + eDate;
+        document.getElementById("download").style.visibility = "visible";
         createReport();
     }
+
 }
 
 function createReport() {
@@ -86,26 +88,21 @@ function createReport() {
         document.getElementById("mLight").textContent = "Mean light level: " + newData.mLight + "lx";
         document.getElementById("mSound").textContent = "Mean sound level: " + newData.mNoise +  "dB";
     })
-    $.get('http://localhost:5000/api/envMeans/' + sDate + '/' + eDate , (newData) => {
-        document.getElementById("mTemp").textContent = "Mean temperature: " + newData.mTemp + "°C";
-        document.getElementById("mLight").textContent = "Mean light level: " + newData.mLight + "lx";
-        document.getElementById("mSound").textContent = "Mean sound level: " + newData.mNoise +  "dB";
-    })
     $.get('http://localhost:5000/api/zoneCount/' + sDate + '/' + eDate , (newData) => {
         zoneCount = newData.noOfZones;
-        document.getElementById("noOfZones").textContent = "Number of zones:" + zoneCount;
-        if (zoneCount > 0)
-        {
-            $.get('http://localhost:5000/api/zoneDayData/' + sDate + '/' + eDate , (newData) => {
-            zDData = newData;
-            google.charts.load("current", {packages:["corechart", "line"]});
-            google.charts.setOnLoadCallback(tempMeansChart);
-            google.charts.load("current", {packages:["corechart", "line"]});
-            google.charts.setOnLoadCallback(lightMeansChart);
-            google.charts.load("current", {packages:["corechart", "line"]});
-            google.charts.setOnLoadCallback(soundMeansChart);
-            })
+        if (zoneCount == null) {
+            zoneCount = 0;
         }
+        document.getElementById("noOfZones").textContent = "Number of zones: " + zoneCount;
+        $.get('http://localhost:5000/api/zoneDayData/' + sDate + '/' + eDate , (newData) => {
+        zDData = newData;
+        google.charts.load("current", {packages:["corechart", "line"]});
+        google.charts.setOnLoadCallback(tempMeansChart);
+        google.charts.load("current", {packages:["corechart", "line"]});
+        google.charts.setOnLoadCallback(lightMeansChart);
+        google.charts.load("current", {packages:["corechart", "line"]});
+        google.charts.setOnLoadCallback(soundMeansChart);
+        })
     })
     $.get('http://localhost:5000/api/doorCount/' + sDate + '/' + eDate , (newData) => {
         document.getElementById("noOfDoors").textContent = "Number of doors: " + newData.noOfDoors;
@@ -186,30 +183,6 @@ function movementCountChart() {
 
 
 function meansChart(type, title, yTitle) {
-    var cData = new google.visualization.DataTable();
-    cData.addColumn("datetime", "Date")
-    for (var i = 1; i <= zoneCount; i++) { //Assumption zoneIDs are from 1 onwards and no numbers are skipped
-        cData.addColumn("number", String(i));
-    }
-    var temp = new Array(zoneCount + 1); //+1 is for the date column
-    for (var i = 0; i <= zoneCount; i++) { //Initialise with nulls in case some zone data is missing for day
-        temp[i] = null;
-    }
-    var prevDate; //Used to track whether a date entry is finished
-    zDData.forEach((entry) => {
-        d = entry.date.split("T")[0].split("-");
-        if (prevDate !== entry.date) {
-            cData.addRows([temp]); //If record has a different day than previous record, means data for previous day is finished and can be added to graph
-            temp = new Array(zoneCount + 1);
-            for (var i = 0; i <= zoneCount; i++) {
-                temp[i] = null;
-            }
-            temp[0] = new Date(d[0], d[1] - 1, d[2]);
-        }
-        temp[entry.zoneID] = entry[type];
-        prevDate = entry.date;
-    })
-    cData.addRows([temp]); //Add last day entry
     var options = {
         title: title,
         tooltip: {
@@ -234,6 +207,34 @@ function meansChart(type, title, yTitle) {
         },
         height: 350
     };
+    var cData = new google.visualization.DataTable();
+    cData.addColumn("datetime", "Date")
+    for (var i = 1; i <= zoneCount; i++) { //Assumption zoneIDs are from 1 onwards and no numbers are skipped
+        cData.addColumn("number", String(i));
+    }
+    if (zoneCount === 0) { //Fix for donwloading report with no data
+        cData.addColumn("number", "No zones");
+        return [cData, options];
+    }
+    var temp = new Array(zoneCount + 1); //+1 is for the date column
+    for (var i = 0; i <= zoneCount; i++) { //Initialise with nulls in case some zone data is missing for day
+        temp[i] = null;
+    }
+    var prevDate; //Used to track whether a date entry is finished
+    zDData.forEach((entry) => {
+        d = entry.date.split("T")[0].split("-");
+        if (prevDate !== entry.date) {
+            cData.addRows([temp]); //If record has a different day than previous record, means data for previous day is finished and can be added to graph
+            temp = new Array(zoneCount + 1);
+            for (var i = 0; i <= zoneCount; i++) {
+                temp[i] = null;
+            }
+            temp[0] = new Date(d[0], d[1] - 1, d[2]);
+        }
+        temp[entry.zoneID] = entry[type];
+        prevDate = entry.date;
+    })
+    cData.addRows([temp]); //Add last day entry
     return [cData, options];
 }
 
@@ -367,12 +368,12 @@ function doorHistoryChart() {
 function download() {
     var pdf = new jsPDF(); //Create pdf, default A4 size
     var elements = ["sDate", "eDate", "noOfUsers", "meanUsers", "minUsers", "maxUsers", "noOfZones", "mTemp", "mLight", "mSound"];
-    var elementsY = [20, 25, 35, 40, 45, 50, 135, 140, 145, 150];
-    var ims = [uTChartI, mCChartI, dCChartI, tMChartI, lMChartI, sMChartI];
-    var imsX = [100, 150, 10, 10, 10, 10];
-    var imsY = [20, 20, 55, 155, 10, 90];
-    var imsXS = [50, 50, 200, 200, 200, 200];
-    var imsYS = [50, 50, 75, 75, 75, 75];
+    var elementsY = [20, 25, 35, 40, 45, 50, 116, 121, 126, 131];
+    var ims = [uTChartI, mCChartI, dCChartI, tMChartI, lMChartI];
+    var imsX = [100, 150, 10, 10, 10];
+    var imsY = [20, 20, 55, 131, 187];
+    var imsXS = [50, 50, 190, 190, 190];
+    var imsYS = [50, 50, 56, 56, 56];
     pdf.setFont("Helvetica", "bold");
     pdf.setFontSize(15);
     pdf.text(document.getElementById("rHeading").textContent, 105, 10, "center"); //Add text at position x, y
@@ -381,17 +382,12 @@ function download() {
     for (var i = 0; i < elements.length; i++) {
         pdf.text(document.getElementById(elements[i]).textContent, 10, elementsY[i]);
     }
-    for (var i = 0; i < ims.length - 2; i++) {
+    for (var i = 0; i < ims.length; i++) {
         pdf.addImage(ims[i], "png", imsX[i], imsY[i], imsXS[i], imsYS[i]); //Add image at position x, y with sizeX, sizeY
     }
     pdf.addPage();
-    for (var i = 0; i < elements.length; i++) {
-        pdf.text(document.getElementById(elements[i]).textContent, 10, elementsY[i]);
-    }
-    for (var i = ims.length - 2; i < ims.length; i++) {
-        pdf.addImage(ims[i], "png", imsX[i], imsY[i], imsXS[i], imsYS[i]); //Add image at position x, y with sizeX, sizeY
-    }
-    pdf.text(document.getElementById("noOfDoors").textContent, 10, 170);
-    pdf.addImage(dHChartI, 10, 175, 200, 75);
+    pdf.addImage(sMChartI, "png", 10, 10, 190, 56);
+    pdf.text(document.getElementById("noOfDoors").textContent, 10, 71);
+    pdf.addImage(dHChartI, 10, 71, 190, 56);
     pdf.save("report.pdf");
 }         
